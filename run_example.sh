b4ff87775c2673b90df8ce59b6f8ea528751784f
yyppyy <yuyanpeng@pku.edu.cn>
2022-01-16 16:16:39 -0500

    mnt tmpfs & disable addr rand in run script

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,8 @@
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd) && export LD_LIBRARY_PATH
+sudo umount /mnt/yanpeng/db
+sudo mount -t tmpfs -o size=8192m tmpfs /mnt/yanpeng/db
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+
 cd ./examples
 make simple_example && cp simple_example test_mltthrd
 ./test_mltthrd $1 $2 /test_program_log/ycsb/load_workload$3.dat /test_program_log/ycsb/run_workload$3.oplist

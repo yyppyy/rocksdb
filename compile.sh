2d504cf2e451a57c8300be076499978ddf7dd4a3
yyppyy <yuyanpeng@pku.edu.cn>
2022-01-15 23:01:30 -0500

    compile sharedlib -> compile app statically

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-make clean && make static_lib && cd examples && make simple_example && cp simple_example test_mltthrd
+make clean -j && make shared_lib -j && sudo -E make install-shared && cd examples && make simple_example && cp simple_example test_mltthrd
